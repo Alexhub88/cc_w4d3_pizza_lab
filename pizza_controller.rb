c81a625c08374f4496e52003f2b6c097c599c3fa
Alexhub88 <alexscott88@hotmail.co.uk>
2018-03-28 22:47:46 +0100

    Second commit: comments in controller file.

--- pizza_controller.rb
+++ pizza_controller.rb
@@ -3,49 +3,47 @@ require( 'sinatra/contrib/all' )
 require( 'pry-byebug' )
 require_relative('./models/pizza_order')
 
-# index
+# index  - shows the list of all the pizzas
 get '/pizza_orders' do
   @orders = PizzaOrder.all()
   erb(:index)
 end
 
-# new
+# new - allows the user to create a new pizza
 get '/pizza_orders/new' do
   erb(:new)
 end
 
-# show
+# show - displays the details of a pizza order chosen by pizza ID
 get '/pizza_orders/:id' do
   @order = PizzaOrder.find(params[:id].to_i)
   erb(:show)
 end
 #
-# # create
+#  create - saves a new pizza order to the database
 post '/pizza_orders' do
   @order = PizzaOrder.new(params)
   @order.save()
   erb(:create)
 end
 #
-# edit
+# edit - allows the user to edit the details of a pizza order
 get '/pizza_orders/:id/edit' do
   @order = PizzaOrder.find(params[:id].to_i)
   erb(:edit)
-  # @order.update()
-  # erb(:update)
 end
 
-# update
+# update - updates the database with the details from the Edit form
 post '/pizza_orders/:id' do
   @order = PizzaOrder.new(params)
   @order.update()
   redirect "/pizza_orders/#{params[:id]}"
 end
 
-# delete
+# destroy - deletes a pizza order from the database
 post '/pizza_orders/:id/delete' do
   @order = PizzaOrder.find(params[:id].to_i)
   @order.delete()
 
- redirect "/pizza_orders"
+ redirect "/pizza_orders"  # redirects the user to the pizza order form
 end
